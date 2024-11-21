d8f910f7798679a9c929698def9e9161dc45503b
JS <46318711+Jioani@users.noreply.github.com>
2024-11-21 08:29:25 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -18,6 +18,12 @@ sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-usb-hid kmod-mmc kmod-sdh
 
 mv -f tmp/r81* feeds/kiddin9/
 
-sed -i 's/256/1024/g' target/linux/x86/image/Makefile
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.6.4/g' package/base-files/files/bin/config_generate
 
+# 更改默认 Shell 为 zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+# TTYD 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
